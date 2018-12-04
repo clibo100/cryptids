89676367a51bd290c4e5f176efaff7eb3b537b1d
Sierra Clibourne <clibo100@mail.chapman.edu>
2018-12-03 21:38:11 -0800

    Finished up searches

--- src/Main.java
+++ src/Main.java
@@ -93,8 +93,14 @@ public class Main {
                 searchSightings();
                 break;
             case 5:
+                searchMedia();
+                break;
             case 6:
+                searchFolklore();
+                break;
             case 7:
+                searchEvidence();
+                break;
             default:
                 break;
 
@@ -420,20 +426,20 @@ public class Main {
             }
 
             String[] names = {"Cryptid ID", "Latitude", "Longitude", "Date Seen", "Viewer ID"};
-            String[][] publications = new String[count][5];
+            String[][] sightings = new String[count][5];
             int i = 0;
             assert getRS != null;
             while (getRS.next())
             {
-                publications[i][0] = Integer.toString(getRS.getInt(1));
-                publications[i][1] = Float.toString(getRS.getFloat(2));
-                publications[i][2] = Float.toString(getRS.getFloat(3));
-                publications[i][3] = String.valueOf(getRS.getDate(4));
-                publications[i][4] = Integer.toString(getRS.getInt(5));
+                sightings[i][0] = Integer.toString(getRS.getInt(1));
+                sightings[i][1] = Float.toString(getRS.getFloat(2));
+                sightings[i][2] = Float.toString(getRS.getFloat(3));
+                sightings[i][3] = String.valueOf(getRS.getDate(4));
+                sightings[i][4] = Integer.toString(getRS.getInt(5));
                 i++;
             }
-            TextTable publicationsTable = new TextTable(names, publications);
-            publicationsTable.printTable();
+            TextTable sightingsTable = new TextTable(names, sightings);
+            sightingsTable.printTable();
         }
 
 
@@ -441,6 +447,281 @@ public class Main {
             e.printStackTrace();
         }
     }
+
+    private static void searchMedia()
+    {
+        Scanner scanner = new Scanner(System.in);
+        Connection conn = dbconnect.getConn();
+        int count = 0;
+        PreparedStatement countPS, pstmt;
+        ResultSet getRS = null, countRS;
+        System.out.println("What would you like to search by?\n" +
+                "1. Cryptid ID\n" +
+                "2. Title\n" +
+                "3. Year\n" +
+                "4. Format\n" +
+                "5. Rating");
+        int response = scanner.nextInt();
+        System.out.print("Enter search term:");
+        scanner.nextLine();
+        String query = scanner.nextLine();
+
+        try
+        {
+            if(response == 1)
+            {
+                countPS = conn.prepareStatement("SELECT  COUNT(*) FROM media WHERE CID = ?");
+                countPS.setInt(1, Integer.parseInt(query));
+                countRS = countPS.executeQuery();
+                countRS.next();
+                count = countRS.getInt(1);
+
+                pstmt = conn.prepareStatement("SELECT * FROM media WHERE CID = ?");
+                pstmt.setInt(1, Integer.parseInt(query));
+                getRS = pstmt.executeQuery();
+            }
+            else if (response == 2)
+            {
+                countPS = conn.prepareStatement("SELECT COUNT(*) FROM media WHERE Title = ?");
+                countPS.setString(1, query);
+                countRS = countPS.executeQuery();
+                countRS.next();
+                count = countRS.getInt(1);
+
+                pstmt = conn.prepareStatement("SELECT * FROM media WHERE Title = ?");
+                pstmt.setString(1, query);
+                getRS = pstmt.executeQuery();
+            }
+            else if (response == 3)
+            {
+                countPS = conn.prepareStatement("SELECT COUNT(*) FROM media WHERE Year = ?");
+                countPS.setInt(1, Integer.parseInt(query));
+                countRS = countPS.executeQuery();
+                countRS.next();
+                count = countRS.getInt(1);
+
+                pstmt = conn.prepareStatement("SELECT * FROM media WHERE Year = ?");
+                pstmt.setInt(1, Integer.parseInt(query));
+                getRS = pstmt.executeQuery();
+            }
+            else if (response == 4)
+            {
+                countPS = conn.prepareStatement("SELECT COUNT(*) FROM media WHERE Format = ?");
+                countPS.setString(1, query);
+                countRS = countPS.executeQuery();
+                countRS.next();
+                count = countRS.getInt(1);
+
+                pstmt = conn.prepareStatement("SELECT * FROM media WHERE Format = ?");
+                pstmt.setString(1, query);
+                getRS = pstmt.executeQuery();
+            }
+            else if (response == 5)
+            {
+                countPS = conn.prepareStatement("SELECT  COUNT(*) FROM media WHERE Rating = ?");
+                countPS.setInt(1, Integer.parseInt(query));
+                countRS = countPS.executeQuery();
+                countRS.next();
+                count = countRS.getInt(1);
+
+                pstmt = conn.prepareStatement("SELECT * FROM media WHERE Rating = ?");
+                pstmt.setInt(1, Integer.parseInt(query));
+                getRS = pstmt.executeQuery();
+            }
+
+            String[] names = {"Cryptid ID", "Title", "Year", "Format", "Rating"};
+            String[][] media = new String[count][5];
+            int i = 0;
+            assert getRS != null;
+            while (getRS.next())
+            {
+                media[i][0] = Integer.toString(getRS.getInt(1));
+                media[i][1] = getRS.getString(2);
+                media[i][2] = Integer.toString(getRS.getInt(3));
+                media[i][3] = getRS.getString(4);
+                media[i][4] = Integer.toString(getRS.getInt(5));
+                i++;
+            }
+            TextTable mediaTable = new TextTable(names, media);
+            mediaTable.printTable();
+        }
+
+
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void searchFolklore()
+    {
+        Scanner scanner = new Scanner(System.in);
+        Connection conn = dbconnect.getConn();
+        int count = 0;
+        PreparedStatement countPS, pstmt;
+        ResultSet getRS = null, countRS;
+        System.out.println("What would you like to search by?\n" +
+                "1. Cryptid ID\n" +
+                "2. Format\n" +
+                "3. Year\n" +
+                "4. Location");
+        int response = scanner.nextInt();
+        System.out.print("Enter search term:");
+        scanner.nextLine();
+        String query = scanner.nextLine();
+
+        try
+        {
+            if(response == 1)
+            {
+                countPS = conn.prepareStatement("SELECT  COUNT(*) FROM folklore WHERE CID = ?");
+                countPS.setInt(1, Integer.parseInt(query));
+                countRS = countPS.executeQuery();
+                countRS.next();
+                count = countRS.getInt(1);
+
+                pstmt = conn.prepareStatement("SELECT * FROM folklore WHERE CID = ?");
+                pstmt.setInt(1, Integer.parseInt(query));
+                getRS = pstmt.executeQuery();
+            }
+            else if (response == 2)
+            {
+                countPS = conn.prepareStatement("SELECT COUNT(*) FROM folklore WHERE Folklore = ?");
+                countPS.setString(1, query);
+                countRS = countPS.executeQuery();
+                countRS.next();
+                count = countRS.getInt(1);
+
+                pstmt = conn.prepareStatement("SELECT * FROM folklore WHERE Folklore = ?");
+                pstmt.setString(1, query);
+                getRS = pstmt.executeQuery();
+            }
+            else if (response == 3)
+            {
+                countPS = conn.prepareStatement("SELECT COUNT(*) FROM folklore WHERE Year = ?");
+                countPS.setInt(1, Integer.parseInt(query));
+                countRS = countPS.executeQuery();
+                countRS.next();
+                count = countRS.getInt(1);
+
+                pstmt = conn.prepareStatement("SELECT * FROM folklore WHERE Year = ?");
+                pstmt.setInt(1, Integer.parseInt(query));
+                getRS = pstmt.executeQuery();
+            }
+            else if (response == 4)
+            {
+                countPS = conn.prepareStatement("SELECT COUNT(*) FROM folklore WHERE Location = ?");
+                countPS.setString(1, query);
+                countRS = countPS.executeQuery();
+                countRS.next();
+                count = countRS.getInt(1);
+
+                pstmt = conn.prepareStatement("SELECT * FROM folklore WHERE Location = ?");
+                pstmt.setString(1, query);
+                getRS = pstmt.executeQuery();
+            }
+
+            String[] names = {"Cryptid ID", "Format", "Year", "Location"};
+            String[][] folklore = new String[count][4];
+            int i = 0;
+            assert getRS != null;
+            while (getRS.next())
+            {
+                folklore[i][0] = Integer.toString(getRS.getInt(1));
+                folklore[i][1] = getRS.getString(2);
+                folklore[i][2] = Integer.toString(getRS.getInt(3));
+                folklore[i][3] = getRS.getString(4);
+                i++;
+            }
+            TextTable folkloreTable = new TextTable(names, folklore);
+            folkloreTable.printTable();
+        }
+
+
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void searchEvidence()
+    {
+        Scanner scanner = new Scanner(System.in);
+        Connection conn = dbconnect.getConn();
+        int count = 0;
+        PreparedStatement countPS, pstmt;
+        ResultSet getRS = null, countRS;
+        System.out.println("What would you like to search by?\n" +
+                "1. Cryptid ID\n" +
+                "2. Date (YYYY-MM-DD)\n" +
+                "3. Location");
+        int response = scanner.nextInt();
+        System.out.print("Enter search term:");
+        scanner.nextLine();
+        String query = scanner.nextLine();
+
+        try
+        {
+            if(response == 1)
+            {
+                countPS = conn.prepareStatement("SELECT  COUNT(*) FROM evidence WHERE CID = ?");
+                countPS.setInt(1, Integer.parseInt(query));
+                countRS = countPS.executeQuery();
+                countRS.next();
+                count = countRS.getInt(1);
+
+                pstmt = conn.prepareStatement("SELECT * FROM evidence WHERE CID = ?");
+                pstmt.setInt(1, Integer.parseInt(query));
+                getRS = pstmt.executeQuery();
+            }
+            else if (response == 2)
+            {
+                countPS = conn.prepareStatement("SELECT COUNT(*) FROM evidence WHERE 'Date' = ?");
+                SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+                java.util.Date utilDate = format.parse(query);
+                java.sql.Date sqlDate = new java.sql.Date(utilDate.getTime());
+                countPS.setDate(1, sqlDate);
+                countRS = countPS.executeQuery();
+                countRS.next();
+                count = countRS.getInt(1);
+
+                pstmt = conn.prepareStatement("SELECT * FROM evidence WHERE 'Date' = ?");
+                pstmt.setDate(1, sqlDate);
+                getRS = pstmt.executeQuery();
+            }
+            else if (response == 3)
+            {
+                countPS = conn.prepareStatement("SELECT COUNT(*) FROM evidence WHERE Location = ?");
+                countPS.setString(1, query);
+                countRS = countPS.executeQuery();
+                countRS.next();
+                count = countRS.getInt(1);
+
+                pstmt = conn.prepareStatement("SELECT * FROM evidence WHERE Location = ?");
+                pstmt.setString(1, query);
+                getRS = pstmt.executeQuery();
+            }
+
+            String[] names = {"Cryptid ID", "Date", "Type", "Location"};
+            String[][] evidence = new String[count][4];
+            int i = 0;
+            assert getRS != null;
+            while (getRS.next())
+            {
+                evidence[i][0] = Integer.toString(getRS.getInt(1));
+                evidence[i][1] = String.valueOf(getRS.getDate(2));
+                evidence[i][2] = getRS.getString(3);
+                evidence[i][3] = getRS.getString(4);
+                i++;
+            }
+            TextTable evidenceTable = new TextTable(names, evidence);
+            evidenceTable.printTable();
+        }
+
+
+        catch (SQLException | ParseException e) {
+            e.printStackTrace();
+        }
+    }
+
 
     private static void printTable()
     {
